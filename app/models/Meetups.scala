a5914b3636fdca1bdaae07ea38d7e1d4d8acc531
Joost Heijkoop <joostheijkoop@gmail.com>
2016-02-04 20:47:13 +0100

    Added basic persistence of meetups

--- app/models/Meetups.scala
+++ app/models/Meetups.scala
@@ -1,11 +1,38 @@
 package models
 
+import java.io.{PrintWriter, File}
+
 object Meetups {
   type Meetup = (String, String)
-  private var meetups = Map.empty[String, String]
+  val storeName = "store.csv"
+  private var meetups = fromDisk
+
+  def fromDisk: Map[String, String] = {
+    Option(scala.io.Source.fromFile(storeName))
+      .map { store =>
+        store
+          .mkString
+          .split('\n')
+          .filter(_.trim.nonEmpty)
+          .map(row => row.split(',').toList)
+          .flatMap(values => values.headOption.zip(values.tail.headOption))
+          .toMap
+      }
+        .getOrElse(Map.empty)
+  }
 
   def add(meetup: Meetup) = {
     meetups = meetups + meetup
+
+    val store = new PrintWriter(storeName)
+    try {
+      store.write(
+        meetups
+          .map(meetup => List(meetup._1, meetup._2).mkString(","))
+          .mkString("\n")) + "\n"
+    } finally {
+      store.close()
+    }
   }
 
   def list = meetups
