b8f6aa18cffd43be71fc975cc14d5c54eb2303b8
Joost Heijkoop <joostheijkoop@gmail.com>
2016-02-04 19:50:24 +0100

    Created add meetups page

--- app/controllers/Main.scala
+++ app/controllers/Main.scala
@@ -1,9 +1,29 @@
 package controllers
 
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{AnyContent, Request, Action, Controller}
+
+import scala.collection.mutable
 
 object Main extends Controller {
+  var meetups = Map.empty[String, String]
   def index = Action {
     Ok(views.html.index())
+  }
+
+  def create = Action { (request: Request[AnyContent]) =>
+    if (request.method == "POST") {
+      val form = request.body.asFormUrlEncoded.get
+      val name = form("name").head
+      val description = form("description").head
+
+      meetups = meetups + (name -> description)
+
+      println(meetups)
+
+      Redirect("/create")
+    } else {
+      Ok(views.html.create())
+    }
+
   }
 }
